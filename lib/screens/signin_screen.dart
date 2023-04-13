63182dada0661ba32626c4cbb68f582eacc146f3
Aman <optimisticaman25@gmail.com>
2023-04-13 19:25:29 +0530

    initial commit

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,74 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutterapp/reusable_widgets/reusable_widgets.dart';
+import 'package:flutterapp/screens/signup_screen.dart';
+import 'package:flutterapp/utils/color_utils.dart';
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController= TextEditingController();
+  TextEditingController _emailTextController= TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+
+                gradient: LinearGradient(colors:[
+                  hexStringToColor("FFFFFF"),
+                  hexStringToColor("FFFFFF")
+                ], begin: Alignment.topCenter,end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding:EdgeInsets.fromLTRB(
+                20,MediaQuery.of(context).size.height*0.2,20,0),
+              child:Column(
+                children: <Widget>[
+                  logoWidget("assets/images/logo1.jpg"),
+                  SizedBox(
+                    height:30,
+                  ),
+                  reusableTextField("Enter UserName", Icons.person_outline, false, _emailTextController),
+                SizedBox(
+                height:20,
+          ),
+                  reusableTextField("Enter Password", Icons.person_outline, false, _passwordTextController),
+                  SizedBox(
+                    height: 20,
+                  ),
+              firebaseUIButton(context, "Login", (){}),
+              signUpOption()
+                ],
+              ),
+            ),
+          ),
+        ),);
+  }
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account?",
+            style: TextStyle(color: Colors.black)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/screens/home_screen.dart';
+
+import '../reusable_widgets/reusable_widgets.dart';
+import '../utils/color_utils.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _userNameTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Sign Up",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                hexStringToColor("FFFFFF"),
+                hexStringToColor("FFFFFF")
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    reusableTextField("Email Address", Icons.person_outline, false,
+                        _userNameTextController),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    reusableTextField("Password", Icons.person_outline, false,
+                        _emailTextController),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    reusableTextField("Password Confirmation", Icons.lock_outlined, true,
+                        _passwordTextController),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    firebaseUIButton(context, "I'll send you a confirmation code", () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+
+                    })
+                  ],
+                ),
+              ))),
+    );
+  }
+}
